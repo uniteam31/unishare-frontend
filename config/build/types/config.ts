306c2d5fdb4ed7885cb0454f22d16a1c9e8ebfb2
Daniil <temnay2@icloud.com>
2024-10-12 21:59:11 +0300

    configured webpack

---
+++ config/build/types/config.ts
@@ -0,0 +1,16 @@
+export type BuildMode = 'development' | 'production';
+
+export interface BuildPaths {
+	entry: string;
+	build: string;
+	html: string;
+	src: string;
+}
+
+export interface BuildOptions {
+	mode: BuildMode;
+	paths: BuildPaths;
+	port: number;
+	isDev: boolean;
+	apiUrl: string;
+}
