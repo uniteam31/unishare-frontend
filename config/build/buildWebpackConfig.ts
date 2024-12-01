3c1b50bb8c19e4bf6fae3738769629c34f152ebf
Daniil <temnay2@icloud.com>
2024-12-01 19:09:39 +0300

    fix webpack warnings

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -24,6 +24,11 @@ export const BuildWebpackConfig = (options: BuildOptions): WebpackConfiguration 
 		mode: mode,
 		plugins: BuildPlugins(options),
 		devServer: BuildDevServer(options),
+		ignoreWarnings: [
+			{
+				message: /Deprecation/,
+			},
+		],
 	};
 };
 
