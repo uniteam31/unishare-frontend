3199060f8f82621cf0704accb0301a39547d92ed
Konstantin Gorshkov <k.gorshkov@vkteam.ru>
2025-01-22 02:10:07 +0300

    added small calendar widget

--- src/pages/CalendarPage/ui/CalendarPage/CalendarPage.tsx
+++ src/pages/CalendarPage/ui/CalendarPage/CalendarPage.tsx
@@ -4,9 +4,11 @@ import interactionPlugin from '@fullcalendar/interaction';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import React, { useEffect } from 'react';
+import Calendar from 'react-calendar';
 import { useNavigationStore } from 'entities/Navigation';
 import { Button } from 'shared/ui';
 import { Divider } from 'shared/ui/Divider/Divider';
+import './calendar.scss';
 import s from './CalendarPage.module.scss';
 
 const CalendarPage = () => {
@@ -22,14 +24,36 @@ const CalendarPage = () => {
 				<Button className={s.createButton}>
 					Новое событие
 				</Button>
+				<Calendar
+					// onChange={setStartDate} value={startDate}
+				/>
+
+				<div>
+					Пространства
+				</div>
+
+				<div className={s.spacesList}>
+					<div className={s.spaceItem}>
+						<input type="checkbox"/>
+						Все пространства
+					</div>
+					<div className={s.spaceItem}>
+						<input type="checkbox" checked={true} />
+						Мое пространство
+					</div>
+					<div className={s.spaceItem}>
+						<input type="checkbox"/>
+						Пространство Алексея Калиниченко
+					</div>
+				</div>
 
 			</div>
 
-			<Divider />
+			<Divider/>
 
 			<div className={s.calendar}>
 				<FullCalendar
-					plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+					plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin ]}
 					initialView={'dayGridMonth'}
 					themeSystem="yeti"
 					headerToolbar={{
@@ -37,6 +61,7 @@ const CalendarPage = () => {
 						center: 'title',
 						right: 'timeGridWeek dayGridMonth timeGridDay'
 					}}
+					height="100%"
 					nowIndicator={true}
 					navLinks={true}
 					firstDay={1}
