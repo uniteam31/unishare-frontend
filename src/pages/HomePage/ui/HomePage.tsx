4c86adc5cfd4ef1695e2bf5f7695fa3196a4f18c
Daniil <temnay2@icloud.com>
2025-03-29 18:15:00 +0300

    feature/UNISHARE-57: update home page

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -5,11 +5,13 @@ import { FriendsWidget } from 'widgets/FriendsWidget';
 import { NotesWidget } from 'widgets/NotesWidget';
 import { SpacesWidget } from 'widgets/SpacesWidget';
 import { useNavigationStore } from 'entities/Navigation';
+import { useGetUserSpaces } from 'entities/Space';
 import { useUserStore } from 'entities/User';
 import s from './HomePage.module.scss';
 
 const HomePage = memo(() => {
 	const { setCurrentService } = useNavigationStore();
+	const { spaces } = useGetUserSpaces();
 
 	// TODO: remove useless code
 	const { authData, initAuthData } = useUserStore();
@@ -31,11 +33,14 @@ const HomePage = memo(() => {
 		};
 	}, [listenRequestInitAuthData]);
 
+	// TODO: в будущем разделять сценарии отсутствия пространств и инициализации
+	const isSpacesAvailable = Boolean(spaces.length);
+
 	return (
 		<div className={s.HomePage}>
-			{!authData?.isInited && <SpacesWidget />}
+			{(!authData?.isInited || !isSpacesAvailable) && <SpacesWidget />}
 
-			{authData?.isInited && (
+			{authData?.isInited && isSpacesAvailable && (
 				<>
 					{/* TODO: выпилить отсюда и из микрофронта, перенести в навбар */}
 					<AccountSettingsWidget />
